c1383835a58d2c7ecaec4d47648c7d2412a4d5bc
blister-pack <andrecrujo@hotmail.com>
2025-06-20 14:54:07 +0200

    change project structure to separate components from pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ import {
 } from "react-router";
 import Header from "./components/Header/Header.jsx";
 import Layout from "./Layout.jsx";
-import Home from "./components/Home/Home.jsx";
-import MyPlants from "./components/MyPlants/MyPlants.jsx";
+import Home from "./pages/Home/Home.jsx";
+import MyPlants from "./pages/MyPlants/MyPlants.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
