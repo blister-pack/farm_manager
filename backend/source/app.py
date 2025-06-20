519fd21601cb0b4310209046080ee9eb4187f729
blister-pack <andrecrujo@hotmail.com>
2025-06-20 22:21:43 +0200

    fastapi setup to connect to frontend

--- backend/source/app.py
+++ backend/source/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -15,6 +17,16 @@ plants = {
         "ideal_humidity": [30, 70],
     },
 }
+
+origins = ["http://localhost:5173/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/plant/{plant_id}")
