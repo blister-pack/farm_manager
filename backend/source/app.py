7c9296f7a4e1b2836c670e07f07489cce5c51566
blister-pack <andrecrujo@hotmail.com>
2025-06-27 20:43:01 +0200

    finished setting up db funcs

--- backend/source/app.py
+++ backend/source/app.py
@@ -5,7 +5,43 @@ from fastapi import FastAPI, Path, Depends, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+from contextlib import asynccontextmanager
 
+# ---------------------------------------------------#
+"""
+DB STUFF
+"""
+
+
+class Plant(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    min_temperature: int = Field(index=True)
+    max_temperature: int = Field(index=True)
+    min_humidity: int = Field(index=True)
+    max_humidity: int = Field(index=True)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# ------------------------------------------------------------#
 
 app = FastAPI()
 
@@ -33,13 +69,10 @@ app.add_middleware(
 )
 
 
-class Plant(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    min_temperature: int = Field(index=True)
-    max_temperature: int = Field(index=True)
-    min_humidity: int = Field(index=True)
-    max_humidity: int = Field(index=True)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
 @app.get("/plants")
